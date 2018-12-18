b824843dd08227620482feeb934f83806ea3176a
Caali <modsfortera@gmail.com>
2018-12-18 18:00:02 +0100

    Add warning when running on unsupported legacy versions of proxy.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = function NoCustomLoadingScreens(mod) {
+    if(mod.proxyAuthor !== 'caali')
+        mod.warn('You are trying to use BattleNotify on an unsupported version of tera-proxy. It may not work as expected, and even if it does now it may break at any point in the future!');
+
     mod.hook('S_LOADING_SCREEN_CONTROL_INFO', 2, event => {
         if (event.enableCustom) {
             event.enableCustom = false;
