530dfa8f1155f43d70b17fa49d9975723f88ed56
Caali <modsfortera@gmail.com>
2018-12-18 18:02:39 +0100

    Fix name.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function NoCustomLoadingScreens(mod) {
     if(mod.proxyAuthor !== 'caali')
-        mod.warn('You are trying to use BattleNotify on an unsupported version of tera-proxy. It may not work as expected, and even if it does now it may break at any point in the future!');
+        mod.warn('You are trying to use NoCustomLoadingScreens on an unsupported version of tera-proxy. It may not work as expected, and even if it does now it may break at any point in the future!');
 
     mod.hook('S_LOADING_SCREEN_CONTROL_INFO', 2, event => {
         if (event.enableCustom) {
