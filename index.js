265dfbaa7ea332c38414dc03901104d1ab1401c3
Caali <modsfortera@gmail.com>
2018-12-24 11:20:57 +0100

    Retardation.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function NoCustomLoadingScreens(mod) {
-    if(mod.proxyAuthor !== 'caali')
+    if(mod.proxyAuthor !== 'caali' || !global.TeraProxy)
         mod.warn('You are trying to use NoCustomLoadingScreens on an unsupported version of tera-proxy. It may not work as expected, and even if it does now it may break at any point in the future!');
 
     mod.hook('S_LOADING_SCREEN_CONTROL_INFO', 2, event => {
