7368e924e3b8a97c39e1905fa25a69f96e57a1fd
Caali <modsfortera@gmail.com>
2019-01-26 01:38:51 +0100

    Update legacy proxy warning.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = function NoCustomLoadingScreens(mod) {
-    if(mod.proxyAuthor !== 'caali' || !global.TeraProxy)
-        mod.warn('You are trying to use NoCustomLoadingScreens on an unsupported version of tera-proxy. It may not work as expected, and even if it does now it may break at any point in the future!');
+    if(mod.proxyAuthor !== 'caali' || !global.TeraProxy) {
+        mod.warn('You are trying to use NoCustomLoadingScreens on an unsupported legacy version of tera-proxy.');
+        mod.warn('The module may not work as expected, and even if it works for now, it may break at any point in the future!');
+        mod.warn('It is highly recommended that you download the latest official version from the #proxy channel in https://discord.gg/dUNDDtw');
+    }
 
     mod.hook('S_LOADING_SCREEN_CONTROL_INFO', 2, event => {
         if (event.enableCustom) {
