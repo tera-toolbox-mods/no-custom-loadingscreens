f8eed7e0341936d270839d3c0a8d83a385875ada
Caali <modsfortera@gmail.com>
2019-05-08 12:15:31 +0200

    Remove warning about legacy versions of tera-proxy as it should be clear that this mod is only compatible with TERA Toolbox.

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 module.exports = function NoCustomLoadingScreens(mod) {
-    if(mod.proxyAuthor !== 'caali' || !global.TeraProxy) {
-        mod.warn('You are trying to use NoCustomLoadingScreens on an unsupported legacy version of tera-proxy.');
-        mod.warn('The module may not work as expected, and even if it works for now, it may break at any point in the future!');
-        mod.warn('It is highly recommended that you download the latest official version from the #proxy channel in https://discord.gg/dUNDDtw');
-    }
-
     mod.hook('S_LOADING_SCREEN_CONTROL_INFO', 2, event => {
         if (event.enableCustom) {
             event.enableCustom = false;
